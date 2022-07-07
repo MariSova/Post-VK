77038ec63fb7e804c7b8fa68fea6b02f33ea73eb
MariSova <bk.1901@bk.ru>
2022-07-07 16:10:35 +0300

    h

--- src/main/kotlin/Post.kt
+++ src/main/kotlin/Post.kt
@@ -1,26 +1,60 @@
 package ru.netology
 
 data class Post(
-    val id: Int = 1,
-    val ownerId: Long = 0,
-    val fromId: Long = 0,
-    val createdBy: Long = 0,
-    val date: Long = 0,
-    val text: String = "Hi",
-    val replyOwnerId: Long = 0,
-    val replyPostId: Long = 0,
-    val friendsOnly: Boolean = false,
-    val comments: Int = 0,
-    val likes: Int = 0,
-    val reposts: Int = 0,
-    val views: Int = 0,
-    val postType: String = "",
-    val signerId: Boolean = true, //тип записи
-    val canPin: Boolean = true,
-    val canEdit: Boolean = true,
-    val isPinned: Boolean = true,
-    val markedAsAds: Boolean = true,
-    val isFavorite: Boolean = true,
-    val donut: String = "",
-    val postponedId: Int = 0,
+    val id: Int,
+    val ownerId: Long,
+    val fromId: Long,
+    val createdBy: Long,
+    val date: Long,
+    val text: String,
+    val replyOwnerId: Long,
+    val replyPostId: Long,
+    val friendsOnly: Boolean ,
+    val comments: Int,
+    val likes: Int,
+    val reposts: Int?,
+    val views: Int?,
+    val postType: String,
+    val signerId: Boolean, //тип записи
+    val canPin: Boolean,
+    val canEdit: Boolean,
+    val isPinned: Boolean,
+    val markedAsAds: Boolean,
+    val isFavorite: Boolean,
+    val donut: String?,
+    val postponedId: Int
 )
+
+open class Likes (
+    val count: Int?,
+    val userLikes: Boolean,
+    val canLike: Boolean,
+    val canPublish: Boolean,
+    val postId: Int?
+
+        )
+ class AttachmentLikes(
+     count: Int?,
+     userLikes: Boolean,
+     canLike: Boolean,
+     canPublish: Boolean,
+     postId: Int?,
+     val original: Likes
+ ): Likes(count, userLikes, canLike, canPublish, postId)
+
+sealed class Attachments(val type: String)
+
+data class ArrayAttachments(
+    val type: String
+){
+    data class Video(val video: Video) : Attachments("video")
+    data class Audio(val audio: Audio) : Attachments("audio")
+    data class Photo(val photo: Photo) : Attachments("photo")
+    data class Document(val document: Document) : Attachments("document")
+}
+fun fooAttachments(att: Attachments): String = when (att) {
+    is ArrayAttachments.Audio -> "audio \nid: 1, \nalbumId: 1, \nownerId: 1, \nuserId: 1"
+    is ArrayAttachments.Document -> "document \nid: 1, \nalbumId: 1, \nownerId: 1, \nuserId: 1"
+    is ArrayAttachments.Photo -> "photo \nid: 1, \nalbumId: 1, \nownerId: 1, \nuserId: 1"
+    is ArrayAttachments.Video -> "video \nid: 1, \nalbumId: 1, \nownerId: 1, \nuserId: 1"
+}
